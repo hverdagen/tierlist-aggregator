53a63e83d2635d8ad821e3f1e3270be5f0ac129b
hverdagen <elst@itu.dk>
2018-03-19 21:09:41 -0700

    see prev (import fetch)

--- tierlist/tier-server/api test.js
+++ tierlist/tier-server/api test.js
@@ -2,6 +2,8 @@ const port = 4200
 const base = 'http://localhost:'+port+'/api/v1/'
 const path = base + 'characters/';
 
+import fetch from 'node-fetch'; //npm install node-fetch --save
+
 console.log('fetch characters');
 fetch(path)
     .then(response => response.json())
